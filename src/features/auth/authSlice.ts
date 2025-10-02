cf388cf6d3ba901ca448eda4da201838ebc43bc7
gavinarori <arorigavin@gmail.com>
2025-10-02 13:35:32 +0300

    added components

--- src/features/auth/authSlice.ts
+++ src/features/auth/authSlice.ts
@@ -31,24 +31,24 @@ const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {},
-  extraReducers: (builder) => {
+  extraReducers: (builder:any) => {
     builder
-      .addCase(login.pending, (state) => {
+      .addCase(login.pending, (state:any) => {
         state.loading = true;
         state.error = undefined;
       })
-      .addCase(login.fulfilled, (state, action: PayloadAction<User>) => {
+      .addCase(login.fulfilled, (state:any, action: PayloadAction<User>) => {
         state.loading = false;
         state.user = action.payload;
       })
-      .addCase(login.rejected, (state, action) => {
+      .addCase(login.rejected, (state:any, action:any) => {
         state.loading = false;
         state.error = action.error.message;
       })
-      .addCase(fetchMe.fulfilled, (state, action: PayloadAction<User>) => {
+      .addCase(fetchMe.fulfilled, (state:any, action: PayloadAction<User>) => {
         state.user = action.payload;
       })
-      .addCase(logout.fulfilled, (state) => {
+      .addCase(logout.fulfilled, (state:any) => {
         state.user = null;
       });
   },
